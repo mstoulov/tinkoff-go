e3d6161eecfad6586653e3eefa64541189e52465
mstoulov <mstoulov@yandex.ru>
2023-09-19 17:32:52 +0300

    [hw4]

--- lesson5/homework/pipeline.go
+++ lesson5/homework/pipeline.go
@@ -11,7 +11,32 @@ type (
 
 type Stage func(in In) (out Out)
 
+func EnableContextClosing(ctx context.Context, in In) Out {
+	out := make(chan any)
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				close(out)
+				for _ = range in {
+				}
+				return
+			case a, ok := <-in:
+				if !ok {
+					close(out)
+					return
+				}
+				out <- a
+			}
+		}
+	}()
+	return out
+}
+
 func ExecutePipeline(ctx context.Context, in In, stages ...Stage) Out {
-	// TODO
-	return nil
+	for _, stage := range stages {
+		out := stage(EnableContextClosing(ctx, in))
+		in = out
+	}
+	return in
 }
